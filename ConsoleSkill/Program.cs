c008a76e77022e5d2a02e6b693a375a9990c6b9c
announsed <epifanov.02@mail.ru>
2023-10-17 20:39:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleSkill/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleSkill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Sergey";
+            Console.WriteLine(MyName);
+
+        }
+    }
+}
