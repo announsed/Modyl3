c2f807d463b77e43cac81adc204d17c6ba752113
announsed <epifanov.02@mail.ru>
2023-10-23 22:24:37 +0300

    Добавлен Q4.1.18

---
+++ Q4.1.18/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Q4._1._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на русском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Твой цвет красный!");
+                    break;
+
+
+                case "зеленый":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Твой цвет зеленый!");
+                    break;
+
+                case "голубой":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Твой цвет голубой!");
+                    break;
+
+                case "желтый":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Твй цвет желтый!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("а в блоке default поменяйте на желтый цвет цвет фона, а цвет текста — на красный.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
