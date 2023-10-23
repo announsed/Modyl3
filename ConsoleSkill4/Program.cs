aa6aefa6220cc3b73d9638066397bd1b4c17cd27
announsed <epifanov.02@mail.ru>
2023-10-23 21:44:27 +0300

    Условные конструкции

---
+++ ConsoleSkill4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleSkill4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5 + 6;
+            var b = 7 + 8;
+
+            var c = (b == a);
+            var d = (b == a);
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
