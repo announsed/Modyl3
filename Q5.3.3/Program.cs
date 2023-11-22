fbaedf687f44ae7e6d81c80868cf04c300d9ef04
announsed <epifanov.02@mail.ru>
2023-11-22 21:16:27 +0300

    Задание 5.5.3

---
+++ Q5.3.3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Q5._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то:");
+            var str = Console.ReadLine();
+            Console.WriteLine("Укажите глубину эха");
+
+            var deep = int.Parse(Console.ReadLine());
+            for (int i = 0; i < deep; i++)
+            {
+                Echo(str, deep);
+            }
+        }
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+            if (modif.Length > 2) 
+            { 
+                modif = modif.Remove(0, 2);
+            }
+            Console.WriteLine("..." + modif);
+            if (deep > 0)
+            {
+                Echo(modif, deep - 1); 
+            }
+        }
+    }
+}
+
+
